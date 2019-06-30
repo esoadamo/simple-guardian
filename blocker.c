b976b94a77004b2089688cc3519d75027df03639
Adam Hlaváček <git@adamhlavacek.com>
2019-06-30 10:37:23 +0200

    blocker now uses ipset instead of directly interfering with iptables

--- blocker.c
+++ blocker.c
@@ -6,6 +6,7 @@
 #include <pwd.h>
 
 #define iptables /sbin/iptables
+#define ipset /sbin/ipset
 #define username "simpleguardian"
 #define indexCommand 1  
 #define indexIp 2
@@ -18,7 +19,8 @@
 |____/|_|\___/ \___|_|\_\___|_|
 ======================================
 Program that uses iptables to block or unblock IP
-Usage: blocker block/unblock IP
+Usage: first init with ./blocker init
+then proceed with blocker block/unblock IP
 
 Is supposed to be executed with root privileges, so as protection from executing by unauthorized user
 the program checks if you are user simpleguardian or root.
@@ -26,13 +28,25 @@ Before blocking IP the program checks if it is not blocked yet to prevent duplic
 
 After compiling, it is recommended to run following commands:
 chown root:root blocker
-chmod +x blocker
+chmod 755 blocker
 chmod u+s blocker
 */
 
 void help(int exitStatus){
-    printf("usage: blocker block/unblock ip\n");
+    printf("usage: init with: blocker init\n");
+    printf("then usage: blocker block/unblock ip\n");
     exit(exitStatus);
+}
+
+void check_requirements() {
+    if (system("iptables -h > /dev/null 2>&1") != 0) {
+        printf("ERROR: iptables command is missing\n");
+        exit(1);
+    }
+    if (system("ipset -h > /dev/null 2>&1") != 0) {
+        printf("ERROR: ipset command is missing\n");
+        exit(1);
+    }
 }
 
 int check_user_valid(){
@@ -45,39 +59,69 @@ int check_user_valid(){
     return !strcmp(username, udetails->pw_name) || !strcmp("root", udetails->pw_name);
 }
 
+int is_already_blocked(char **argv) {
+    /*
+    Checks if passed IP is already blocked
+    Returns 0 if not blocked, 1 if already blocked
+    */
+    char command_check_if_blocked[] = "ipset test simpleguardian %s > /dev/null 2>&1";
+    char *command_check_if_blocked_formatted = (char*)malloc(sizeof(command_check_if_blocked) + sizeof(argv[indexIp]));
+    sprintf(command_check_if_blocked_formatted, command_check_if_blocked, argv[indexIp]);
+    return system(command_check_if_blocked_formatted) == 0;
+}
 
 int main(int argc, char **argv){
+    check_requirements();
+
     if (!check_user_valid()){
         printf("this program is exclusive for user %s or root\n", username);
         exit(1);
     }
+    if (argc < indexCommand + 1){
+        help(0);
+    }
+
     setuid(0);
+
+    if (!strcmp("init", argv[indexCommand])){
+        if (system("ipset list simpleguardian > /dev/null 2>&1") == 0){
+            printf("already inited before, just flushing\n");
+            system("ipset flush simpleguardian > /dev/null 2>&1");
+            exit(0);
+        } else {
+            system("ipset create simpleguardian iphash > /dev/null 2>&1");
+            printf("init complete\n");
+            system("iptables -I INPUT 2 -m set --match-set simpleguardian src -j DROP");
+            exit(0);
+        }
+    }
+
     if (argc < indexIp + 1){
         help(0);
     }
-    if (!strcmp("block",argv[indexCommand])){
-        // Check if the IP is not blocked yet
-        char command_check_if_blocked[] = "iptables -C INPUT -s %s -j DROP > /dev/null 2>&1";
-        char *command_check_if_blocked_formatted = (char*)malloc(sizeof(command_check_if_blocked) + sizeof(argv[indexIp]));
-        sprintf(command_check_if_blocked_formatted, command_check_if_blocked, argv[indexIp]);
-        if (system(command_check_if_blocked_formatted) == 0){
-            printf("%s is already blocked\n", argv[indexIp]);
+    if (!strcmp("block", argv[indexCommand])){
+        if (is_already_blocked(argv)) {
+            printf("this IP is already blocked\n");
             exit(0);
         }
 
-        // And now block it
         printf("blocking %s\n", argv[indexIp]);
-        char command[] = "iptables -A INPUT -s %s -j DROP > /dev/null 2>&1";
+        char command[] = "ipset add simpleguardian %s > /dev/null 2>&1";
         char *command_formatted = (char*)malloc(sizeof(command) + sizeof(argv[indexIp]));
         sprintf(command_formatted, command, argv[indexIp]);
 
         system(command_formatted);
 
         exit(0);
-    } else if (!strcmp("unblock",argv[indexCommand])){
+    } else if (!strcmp("unblock", argv[indexCommand])){
+        if (!is_already_blocked(argv)) {
+            printf("this IP is not blocked\n");
+            exit(0);
+        }
+
         printf("unblocking %s\n", argv[indexIp]);
 
-        char command[] = "iptables -D INPUT -s %s -j DROP > /dev/null 2>&1";
+        char command[] = "ipset del simpleguardian %s > /dev/null 2>&1";
         char *command_formatted = (char*)malloc(sizeof(command) + sizeof(argv[indexIp]));
         sprintf(command_formatted, command, argv[indexIp]);
 
